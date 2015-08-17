d77a8e7268dcbd3038474af58cbae00c710f1ab7
Kelly Knights <kknights@gmail.com>
2015-08-17 18:36:58 -0400

    slide 1, complete
    slide one refactored!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,29 @@
 // jshint devel:true
-console.log('\'Allo \'Allo!');
+// console.log('\'Allo \'Allo!');
+
+
+$(function() {
+	//adds .active to the navigation menu item
+	$(".navbar-toggle").on("click", function () {
+		$(this).toggleClass("active");
+	});
+
+
+	// Smooth scrolling to internal anchor
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
+
+
+
+
+});
